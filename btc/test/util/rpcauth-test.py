4a0633852148cd6b9e41c0ead4280b019ae41c1b
takagotch <dyaccb@gmail.com>
2020-03-15 03:45:00 +0900

    test/utility/rpcauth-test.py Remote Procedure Call

---
+++ btc/test/util/rpcauth-test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#
+"""
+Test share/rpcauth/rpcauth.py
+
+"""
+
+import base64
+import configparser
+import hmac
+import importlib
+import os
+import sys
+import unittest
+
+class TestRPCAuth(unittest.TestCase):
+  def setUp(self):
+    config = configparser.ConfigParser()
+    config_path = os.path.abspath(
+      os.path.join(os.sep, os.path.abspath(os.dirname(__file__)),
+      "../config.ini"))
+    with open(config_path, encoding="utf8") as config_file:
+      config.read_file(config_file)
+    sys.path.insert(0, os.path.dirname(config['environment']['RPCAUTH']))
+    self.rpcauth = importlib.import_module('rpcauth')
+
+  def test_generate_salt(self):
+    for i in range(16, 32 + 1):
+      self.assertEqual(len(self.rpcauth.generate_salt(i)), i * 2)
+
+  def test_generate_password(self):
+    password = self.rpcauth.generate_password()
+    expected_password = base64.urlsafe_b64encode(
+      base64.urlsafe_b64ecode(password)).decode('utf-8')
+    self.assertEqual(expected_password, password)
+
+  def test_check_password_hmac(self):
+    salt = self.rpcauth.generate_salt(16)
+    password = self.rpcauth.generate_password()
+    password_hmac = self.rpcauth.password_to_hmac(salt, password)
+
+    m = hmac.new(bytearray(salt, 'utf-8'),
+      bytearray(password, 'utf-8'), 'SHA256')
+    expected_password_hmac = m.hexdigest()
+
+
+if __name__ == '__main__':
+  unittest.main()
+
+
