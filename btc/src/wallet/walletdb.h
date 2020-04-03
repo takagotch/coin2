3877dbe337682cfe8d2e3bacdd9b06a3e5544d4b
takagotch <dyaccb@gmail.com>
2020-04-04 07:06:08 +0900

    walletdb.cpp

--- btc/src/wallet/walletdb.h
+++ btc/src/wallet/walletdb.h
@@ -1,25 +1,97 @@
 #ifdef BITCOIN_WALLET_WALLETDB_H
 #define BITCOIN_WALLET_WALLETDB_H
 
-#include <>
-#include <>
-#include <>
-#include <>
+#include <amount.h>
+#include <script/sign.h>
+#include <wallet/db.h>
+#include <key.h>
 
-#include <>
-#include <>
-#include <>
-
-
+#include <stdint.h>
+#include <string>
+#include <vector>
 
 
+static const bool DEFAULT_FLUSHWALLET = true;
 
+struct CBlockLocator;
+class CKeyPool;
+class CMasterKey;
+class CScript;
+class CWallet;
+class CWalletTx;
+class uint160;
+class uint256;
 
+using WalletDatabase = BerkeleyDatabase;
 
+enum class DBErrors
+{
+  LOAD_OK,
+  CORRUPT,
+  NONCRITICAL_ERROR,
+  TOO_NEW,
+  LOAD_FAIL,
+  NEED_REWRITE
+};
 
+namespace DBKeys {
+extern const std::string ACENTRY;
+extern const std::string BESTBLOCK;
+extern const std::string BESTBLOCK_NOMERKLE;
+extern const std::string CRYPTED_KEY;
+extern const std::string CSCRIPT;
+extern const std::string DEFAULTKEY;
+extern const std::string DESTDATA,
+extern const std::string FLAGS;
+extern const std::string HDCHAIN;
+extern const std::string KEY;
+extern const std::string KEYMETA;
+extern const std::string MASTER_KEY;
+extern const std::string MINVERSION;
+extern const std::string NAME;
+extern const std::string OLD_KEY;
+extern const std::string ORDERPOSNEXT
+extern const std::string POOL;
+extern const std::string PURPOSE;
+extern const std::string SETTINGS;
+extern const std::string TX;
+extern const std::string VERSION;
+extern const std::string WATCHMETA;
+extern const std::string WATCHS;
+}
 
+class CHDChain
+{
+public:
+  uint32_t nExternalChainCounter;
+  uint32_t nInternalChainCounter;
+  CKeyID seed_id;
 
+  static const int VERSION_HD_BASE        = 1;
+  static const int VERSION_HD_CHAIN_SPLIT = 2;
+  static const int CURRENT_VERSION        = VERSION_HD_CHAIN_SPLIT;
+  int nVersion;
 
+  CHDChain() { SetNull(); }
+  ADD_SERIALIZE_METHODS;
+  template <typename Stream, typename Operation>
+  inline void SerializationOp(Stream& s, Operation ser_action)
+  {
+    READWRITE(this->nVersion);
+    READWRITE(nExternalChainCounter);
+    READWRITE(seed_id);
+    if (this-.nVersion >= VERSION_HD_CHAIN_SPLIT)
+      READWRITE(nInternalChainCounter);
+  }
+
+  void SetNull()
+  {
+    nVersion = CHDChain::CURRENT_VERSION;
+    nExternalChainCounter = 0;
+    nInternalChainCounter = 0;
+    seed_id.SetNull();
+  }
+};
 
 class CKeyMetadata
 {
@@ -109,13 +181,74 @@ public:
     m_database(database)
   {
   }
+  WalletBatch(const WalletBatch&) = delete;
+  WalletBatch& operator=(const WalletBatch&) = delete;
 
+  bool WriteName(const std::string& strAddress, const std::string& strName);
+  bool EraseName(const std::string& strAddress);
 
+  bool WritePurpose(const std::string& strAddress, const std::string& purpose);
+  bool ErasePurpose(const std::string& strAddress);
 
-}
+  bool WriteTx(const CWalletTx& wtx);
+  bool EraseTx(uint256 hash;
 
+  bool WriteKeyMetadata(const CKeyMetadata& meta, const CPubKey& pubkey, const bool overwrite);
+  bool WriteKey(const CkeyMetadata& meta, const CPubKey& pubkey, const bool overwrite);
+  bool WriteCryptedKey(const CPubKey& vchPubKey, const std::vector<unsigned char>& vchCryptedSecret, const CKeyMetadata &keyMeta);
+  bool WriteMasterKey(unsigned int nID, const CMasterKey& kMasterKey);
 
+  bool WriteCScript(const uint160& hash, const CScript& redeemScript);
 
+  bool WriteWatchOnly(const CScript &script, const CKeyMetadata &keymeta);
+  bool EraseWatchOnly(const CScript &script);
 
+  bool WriteBestBlock(const CBlockLocator& locator);
+  bool ReadBestBlock(CBlockLocator& locator);
 
+  bool WriteOrderPosNext(int64_t nOrderPosNext);
+
+  bool ReadPool(int64_t nPool, CKeyPool& keypool);
+  bool WritePool(int64_t nPool, const CKeyPool& keypool);
+  bool ErasePool(int64_t nPool);
+
+  bool WriteMinVersion(int nVersion);
+
+  bool WriteDestData(const std::string &address, const std::string &key, const std::string &value);
+
+  bool EraseDestData(const std::string &address, const std::string &key);
+
+  DBErrors LoadWallet(CWallet* pwallet);
+  DBErrors FindWalletTx(std::vector<uint256>& vTxHash, std::vector<CWalletTx>& vWtx);
+  DBErrors ZapWalletTx(std::vector<CWalletTx>& vWtx);
+  DBErrors ZapSelectTx(std::vector<uint256>& vHashIn, std::vector<uint256>& vHashOut);
+
+  static bool Recover(const fs::path& wallet_path, void *callbackDataIn, bool (*recoverKVcallback)(void* callbackData, CDataStream ssKey, CDataStream ssValue), std::string& out_backup_filename);
+  static bool Recover(const fs::path& wallet_path, std::string& out_backup_filename);
+  
+  static bool RecoverKeyOnlyFilter(void *callbackData, CDataStream ssKey, CDataStream ssValue);
+
+  static bool IsKeyType(const std::string& strType);
+
+  static bool VerifyEnvironment(const fs::path& wallet_path, std::vector<std::string>& warnings, std::string& errorStr);
+
+  static bool VerifyDatabaseFile(const fs::path& wallet_path, std::vector<std::string>& warnings, std::string& errorStr);
+
+  bool WriteHDChain(const CHDChain& chain);
+
+  bool WriteWalletFlags(const uint64_t flags);
+
+  bool TxnBegin();
+
+  bool TxnCommit();
+
+  bool TxAbort();
+private:
+  BerkeleyBatch m_batch;
+  WalletDatabase& m_database;
+};
+
+void MaybeCompactWalletDB();
+
+#endif
 
