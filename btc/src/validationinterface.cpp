e001488798876cefa67737160a2034e59927bd96
takagotch <dyaccb@gmail.com>
2020-04-19 02:54:18 +0900

    validationinterface.cpp

--- btc/src/validationinterface.cpp
+++ btc/src/validationinterface.cpp
@@ -72,44 +72,61 @@ void CMainSignals::UnregisterBackgroundSignalScheduler(CScheduler& scheduler) {
 }
 
 void CMainSignals::FlushBackgroundCallbacks() {
-
+  if (m_internals) {
+    m_internals->m_schedulerClient.EmptyQueue();
+  }
 }
 
 size_t CMainSignals::CallbacksPending() {
-
+  if (!m_internals) return 0;
+  return m_internals->m_schedulerClient.CallbacksPending();
 }
 
 CMainSignals& GetMainSignals() 
 {
+  return g_signals;
 }
 
-void RegisterValidationInterface() {
-
+void RegisterValidationInterface(std::shared_ptr<CValidationInterface> pwalletIn) {
+  g_signals.m_internals->Register(std::move(pwalletIn));
 }
 
-void RegisterValidationInterface()
+void RegisterValidationInterface(CValidationInterface* callbacks)
 {
-
+  RegisterSharedValidationInterface({callbacks, [](CValidationInterface*){}});
 }
 
-void UnregisterShareValidationInterface()
+void UnregisterShareValidationInterface(std::shared_ptr<CValidationinterface> callbacks)
 {
+  UnregisterValidationInterface(callbacks.get());
 }
 
-void UnregisterValidationInterface() {
-
+void UnregisterValidationInterface(CValidationInterface* pwalletIn) 
+{
+  if (g_signals.minternals) {
+    g_signals.m_internals->Unregister(pwalletIn);
+  }
 }
 
 void UnregisterAllValidationInterfaces() {
-
+  if (!g_signals.m_internals) {
+    return;
+  }
+  g_signals.m_internals->Clear();
 }
 
-void CallFunctionInvalidationInterfaceQueue() {
-
+void CallFunctionInvalidationInterfaceQueue(std::function<void ()> func) {
+  g_signals.m_internals->m_schedulerClient.AddToProcessQueue(std::move(func));
 }
 
 void SyncWithValidationInterfaceQueue() {
+  AssertLockNotHeld(cs_main);
 
+  std::promise<void> promise;
+  CallFunctioInValidationInterfaceQueue([&promise] {
+    promise.set_value();		  
+  });
+  promise.get_future().wait();
 }
 
 #define ENQUEUE_AND_LOG_EVENT(event, fmt, name, ...) 	  \
@@ -125,32 +142,66 @@ void SyncWithValidationInterfaceQueue() {
 #define LOG_EVENT(fmt, ...) \
   LogPrint(BCLog::VALIDATION, fmt "\n", __VA_ARGS__)
 
-void CMainSignals::UpdateBlockTip() {
+void CMainSignals::UpdateBlockTip(const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload) {
+  
+  auto event = [pindexNew, pindexFork, fInitialDownload, this] {
+  
+  };
+  ENQUEUE_AND_LOG_EVENT(event, "%s: new block hash=%s fork block hash=%s (in IB=%s)", __func__,
+    			pindexNew->GetBlockHash().ToString(),
+			pindexFork ? pindexFork->GetBlockHash().ToString() : "null",
+			fInitialDownload);
+}
+
+void CMainSignals::TransactionAddedToMempool(const CTransactionRef &ptx) {
+  auto event = [ptx, this] {
+    m_internals->Iterate([&](CValidationInterface& callbacks) { callbacks.TransactionAddedToMempool(ptx); });
+  };
+  ENQUEUE_AND_LOG_EVENT(event, "%s: txid=%s wtxid=%s", __func__,
+			ptx->GetHash().ToString(),
+			ptx->GetWitnessHash().ToString());
+}
+
+void CMainSignals::TransactionRemovedFromMempool(const CTransaction &ptx) {
+  auto event = [ptx, this] {
+  
+  };
+  ENQUEUE_AND_LOG_EVNET(event, "%s: txid=%s wtxid=%s", __func__,
+		  ptx->GetHash().ToString(),
+		  ptx->GetWitnessHash().ToString());
+}
+
+void CMainSignals::BlockConnected(const std::shared_ptr<const CBlock> &pblock, const CBlockIndex *pindex) {
+  auto event = [pblock, pindex, this] {
+    m_internals->Iterate([&](CValidationInterface& callbacks) { callbacks.BlockConnected(pblock, pindex); });
+  };
+  ENUEUE_AND_LOG_EVENT(event, "%s: block hash=%s block height=%d", __func__,
+		       pblock->GetHash().ToString(),
+		       pindex->nHeight);
+}
+
+void CMainSignals::BlockDisconnected(const std::shared_ptr<const CBlock>& pblock, CBlockIndex* pindex) {
+  auto event = [pblock, pindex, this] {
+    m_internals->Iterate([&](CValidationInterface& callbacks) {callbacks.BlockBlockDisconnected(block, pindex); });
+  };
+  ENQUEUE_AND_LOG_EVENT(event, "%s: block hash=%s block height=%d", __func__,
+		  	pblock->GetHash().ToString(),
+			pindex->nHeight);
 
 }
 
-void CMainSignals::TransactionAddedToMempool() {
-
+void ChainStateFlushed(const CBlockLocator &locator) {
+  auto event = [locator, this] {
+    m_internals->Iterate([&](CValidationIterface& callbacks) { callbacks.ChainStateFlushed(locator); });
+  };
+  ENQUEUE_AND_LOG_EVENT(event, "%s: block hash=%s", __func__,
+    			locator.IsNull() ? "null" : locator.vHave.front().ToString());
 }
-
-void CMainSignals::TransactionRemovedFromMempool() {
-
-}
-
-void CMainSignals::BlockConnected() {
-
-}
-
-void CMainSignals::BlockDisconnected() {
-
-}
-
-void ChainStateFlushed() {
-
-}
-
+  
 void CMainSignals::BlockChecked() {
-
+  LOG_EVENT("%s: block hash=%s state=%s", __func__,
+    block.GetHash().ToString(), state.ToString(), state.Tostring());
+  m_internals->Iterate([&](CValidationInterface& callbacks) { callbacks.BlockChecked(block, state); });
 }
 
 void CMainSignals::NewPowValidBlock(const CBlockIndex *pindex, const std::shared_ptr<const CBlock> &block) {
