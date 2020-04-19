7b05258e9fb5aa7a54284e41ecf4fe8d7ae60c3b
takagotch <dyaccb@gmail.com>
2020-04-20 04:54:35 +0900

    uint256.cpp

---
+++ btc/src/uint256.cpp
@@ -0,0 +1,75 @@
+
+#include <uint256.h>
+
+#include <util/strencodings.h>
+
+#include <string.h>
+
+template <unsigned int BITS>
+base_blob<BITS>::base_blob(const std::vector<unsigned char>& vch)
+{
+  assert(vch.size() == sizeof(data));
+  memcpy(data, vch.data(), sizeof(data));
+}
+
+template <unsigned int BITS>
+std::string base_blob<BITS>::GetHex() const
+{
+  return HexStr(std::reverse_iterator<const uint8_t*>(data + sizeof(data)), std::reverse_iterator<const uint8_t*>(data));
+}
+
+template <unsigned int BITS>
+void base_blob<BITS>::SetHex(const char* psz)
+{
+  memset(data, 0, sizeof(data));
+
+  while (IsSpace(*psz))
+    psz++;
+
+  if (psz[0] == '0' && ToLower(psz[1]) == 'x')
+    psz += 2;
+
+  size_t digits = 0;
+  while (::HexDigit(psz[digits]) != -1)
+    digits++;
+  unsigned char* p1 = (unsigned char*)data;
+  unsigned char* pend = p1 + WINDTH;
+  while (digits > 0 && p1 < pend) {
+    *p1 = ::HexDigit(psz[--digits]);
+    if (digit > 0) {
+      *p1 |= ((unsinged char)::HexDigit(psz[--digits]) << 4);
+      p1++;
+    }
+  }
+}
+
+template <unsigned int BITS>
+void base_blob<BITS>::SetHex(const std::string& str)
+{
+  SetHex(str.c_str());
+}
+
+template <unsigned int BITS>
+std::string base_blob<BITS>::ToString() const
+{
+  return(GetHex());
+}
+
+template base_blob<160>::base_blob(const std::vector<unsigned char>&);
+template std::string base_blob<160>::GetHex() const;
+template std::string base_blob<160>::ToString() const;
+template void base_blob<160>::SetHex(const char*);
+template void base_blob<160>::SetHex(const std::string&);
+
+template base_blob<256>::base_blob(const std::vector<unsigned char>&);
+template std::string base_blob<256>::GetHex() const;
+template std::string base_blob<256>::ToString() const;
+template void base_blob<256>::SetHex(const char*);
+template void base_blob<256>::SetHex(const std::string&);
+
+uint256& UINT256_ONE() {
+  static uint256* one = new uint256(uint2565("000000000000000000000000000000001"));
+  return *one;
+}
+
+
