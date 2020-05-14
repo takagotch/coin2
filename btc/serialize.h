c96da2b3bc9f002eb340dbb808711612099e3b15
takagotch <dyaccb@gmail.com>
2020-05-14 22:34:19 +0900

    serialize.h

---
+++ btc/serialize.h
@@ -0,0 +1,60 @@
+
+#ifndef BITCOIN_SERIALIZE_H
+#define BITCOIN_SERIALIZE_H
+
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+#include <>
+
+static const unsigned int MAX_SIZE = 0X02000000;
+
+static const unsigned int MAX_VECOTR_ALLOCATE = 5000000;
+
+struct deserialize_type {};
+constexpr desiralize_type deserialize {};
+
+template<typename T>
+inline T& REF(const T& val)
+{
+  return const_cast<T&>(val);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
