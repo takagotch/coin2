6c6c5b24b40a22a9f48e360937305f27911a4b89
takagotch <dyaccb@gmail.com>
2020-05-15 14:20:18 +0900

    serialize3.h

--- btc/serialize.h
+++ btc/serialize.h
@@ -70,20 +70,149 @@ inline T& REF(const T& val)
 
 
 
+//
+typename<typename I>
+class BigEndian
+{
+protected:
+  I& m_val;
+public:
+  explicit BigEndian(I& val) : m_val(val)
+  {
+    static_assert();
+    static_assert(sizeof(I) == 2 && std::numeric_limits<I>::min() == 0 && std::numeric_limits<I>::max() == std::numeric_limits<uint16_t //... >);
+  }
 
+  template<typename Stream>
+  void Serialize(Stream& s) const
+  {
+    ser_writedata16be(s, m_val);
+  }
 
+  template<typename Stream>
+  void Unserialize(Stream& s)
+  {
+    m_val = ser_readdata16be(s);
+  }
+};
 
+struct CompactSizeFormatter
+{
+  template<typename Stream, typename I>
+  void Unser(Stream& s, I& V)
+  {
+    uint64_t n = ReadCompactSize<Stream>(s);
+    if (n < std::numeric_limits<I>::min() || n > std::numeric_limits<I>::max()) {
+      throw std::ios_base::failure("CompactSize exceeds limits of type");
+    }
+    v = n;
+  }
 
+  template<typename Stream, typename I>
+  void Ser(Stream& s, I V)
+  {
+    static_assert(std::is_unsigned<I>::value, "CompactSize only supported for unsigned integers");
+    static_assert(std::numeric_limits<I>::max() <= std::numeric_limits<uint64_t>::max(), "CompactSize only supports 64-bit integers and "//... );
+  }
+};
 
+template<size Limits>
+class LimitsString
+{
+protected:
+  std::string& string;
+public:
+  explicit LimitedString(std::string& _string) : string(_string) {}
 
+  template<typename Stream>
+  void Unserialize(Stream& s)
+  {
+    size_t size = ReadCompactSize(s); 
+    if (size > Limit) {
+      throw std::ios_base::failure("String length limit exceeded");
+    }
+    string.resize(size);
+    if (size != 0)
+      s.read((char*)string.data(), size);
+  }
+
+  template<typename Stream>
+  void Serialize(Stream& s) const
+  {
+    WriteCompactSize(s, string.size());
+    if (!string.empty())
+      s.write((char*)string.data(), string.size());
+  }
+};
+
+template<typename I>
+BigEndian<I> WrapBigEndian(I& n) { return BigEndian<I>(n); }
+
+//  
+template<class Formatter>
+struct VectorFormatter
+{
+  template<typename Stream, typename V>
+  void Ser(Stream& s, const V& v)
+  {
+    Formatter formatter;
+    WriteCompactSize(s, v.size());
+    for (const typename V::value_type& elem : v) {
+      formatter.Ser(s, elem);
+    }
+  }
+
+  template<typename Stream, typename V>
+  void Unser(Stream& s, V& v)
+  {
+    Formatter formatter;
+    v.clear();
+    size_t size = ReadCompactSize(s);
+    size_t allocated = 0;
+    while (allocated < size){
+      static_assert(sizeof(typename V::value_type) <= MAX_VECTOR_ALLOCATE, "Vector element size too large");
+      allocated = std::min(size, allocated + MAX_VECTOR_ALLOCATE / sizeof(typename V::value_type));
+      v.reserve(allocated);
+      while (v.size() < allocated) {
+        v.emplace_back();
+	formatter.Unser(s, v.back());
+      }
+    }
+  };
+};
+
+// Forward declarations
 
 
 //
 
 
+// unique_ptr
+template<typename Stream, typename T> void Serialize(Stream& os, const std::unique_ptr<const T>& p);
+template<typename Stream, typename T> void Unserialize(Stream& os, std::unique_ptr<const T>& p);
 
 
+//
+template<typename Stream, typename T>
+inline void Serialize(Stream os, const &T a)
+{
+  a.Serialize(os);
+}
 
+template<typename Stream, typename T>
+inline void Unserialize(Stream& is, T&& a)
+{
+  a.Unserialize(is);
+}
+
+struct DefaultFormatter
+{
+  template<typename Stream, typename T>
+  static void Ser(Stream& s, const T& t) { Serialize(s, t); }
+
+  template<typename Stream, typename T>
+  static void Unser(Stream& s, T& t) { Unserialize(s, t); }
+};
 
 // string
 template<typename Stream, typename C>
